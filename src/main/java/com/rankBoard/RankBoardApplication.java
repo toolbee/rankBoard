21c19f629ffd5ede89fe3ce21e42ccc65bf43b9f
toolbee <wlgns309@gmail.com>
2020-07-19 11:02:03 +0900

    springboot merge

---
+++ src/main/java/com/rankBoard/RankBoardApplication.java
@@ -0,0 +1,13 @@
+package com.rankBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RankBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rankBoard/RankBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rankBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
