431c8c14b205681265773f9c81e074ec65171923
toolbee <wlgns309@gmail.com>
2020-08-03 16:03:54 +0900

    record update, jar ==> war
    -전적 수정
    - jar 배포에서 war 배포로 수정

--- src/main/java/com/rankBoard/RankBoardApplication.java
+++ src/main/java/com/rankBoard/RankBoardApplication.java
@@ -2,10 +2,16 @@ package com.rankBoard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RankBoardApplication {
+public class RankBoardApplication extends SpringBootServletInitializer {
 
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RankBoardApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RankBoardApplication.class, args);
 	}
